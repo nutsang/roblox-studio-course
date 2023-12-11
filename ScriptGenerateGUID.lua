ca26976364e3754cc476db721a08517d55a68732
NutSang Official <90796502+nutsang@users.noreply.github.com>
2023-12-11 20:30:20 +0700

    Delete ScriptGenerateGUID.lua

--- ScriptGenerateGUID.lua
+++
@@ -1,6 +0,0 @@
-local HttpService = game:GetService("HttpService")
-local resultOfWarpInCurlyBracesTrue = HttpService:GenerateGUID(true)
-local resultOfWarpInCurlyBracesFalse = HttpService:GenerateGUID(false)
-
-print("resultOfWarpInCurlyBracesTrue: " .. resultOfWarpInCurlyBracesTrue)
-print("resultOfWarpInCurlyBracesFalse: " .. resultOfWarpInCurlyBracesFalse)
